4bf3dbcd07c133b101dbd60813c097391fb5dddc
Justina <j.klimaite.91@gmail.com>
2020-04-17 13:24:53 +0300

    header desktop

--- js/header.js
+++ js/header.js
@@ -1,40 +1,28 @@
 "use strict";
 
-function renderNav() {
+function renderHeader() {
+    let HTML = '';
+
+    HTML = `<div class="header">
+        <nav class="nav-bar col-12">
+            <img src="./img/logo-2.png">
+            <div class="nav-links">
+            </div>
+        </nav>
+    </div>`;
+
+    return document.querySelector('header').innerHTML = HTML;
+}
+
+function renderNav(nav) {
     let HTML = '';
     
     for ( let i=0; i<nav.length; i++ ) {
-        let navData = nav[i];
-        HTML += nav[i];
-        
+        HTML += `<a href="#">${nav[i]}</a>`
     }
-    return document.querySelector('nav-links').innerHTML = HTML;
+    return document.querySelector('.nav-links').innerHTML = HTML;
 }
 
-    function renderHeader() {
-        let HTML = '';
 
-        HTML = `<div class="header">
-            <nav class="nav-bar col-12">
-                <img src="./img/logo-2.png">
-                <div class="nav-links">
-                    <a href="#">${nav[0]}</a>
-                    <a href="#">${nav[1]}</a>
-                    <a href="#">${nav[2]}</a>
-                    <a href="#">${nav[3]}</a>
-                    <a href="#">${nav[4]}</a>
-                    <a href="#">${nav[5]}</a>
-                    <a href="#">${nav[6]}</a>
-                </div>
-            </nav>
-        </div>`;
-
-    
-        document.querySelector('header').innerHTML = HTML;
-
-        return HTML;
-    }
-   
-
-
-renderHeader(nav);
+renderHeader();
+renderNav(nav)
