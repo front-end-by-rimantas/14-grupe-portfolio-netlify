54b8c3d2076f428155aba088843ad35b1311a1e4
Justina <j.klimaite.91@gmail.com>
2020-04-17 12:28:59 +0300

    loop add

--- js/header.js
+++ js/header.js
@@ -1,26 +1,40 @@
 "use strict";
 
-function renderHeader() {
+function renderNav() {
     let HTML = '';
-
-    HTML = `<div class="header">
-                <nav class="nav-bar col-12">
-                    <img src="./img/logo-2.png">
-                    <div class="nav-links">
-                        <a href="#">${nav}</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                        <a href="#">link</a>
-                    </div>
-                </nav>
-            </div>`;
-
-            document.querySelector('header').innerHTML = HTML;
-
-    return HTML;
+    
+    for ( let i=0; i<nav.length; i++ ) {
+        let navData = nav[i];
+        HTML += nav[i];
+        
+    }
+    return document.querySelector('nav-links').innerHTML = HTML;
 }
+
+    function renderHeader() {
+        let HTML = '';
+
+        HTML = `<div class="header">
+            <nav class="nav-bar col-12">
+                <img src="./img/logo-2.png">
+                <div class="nav-links">
+                    <a href="#">${nav[0]}</a>
+                    <a href="#">${nav[1]}</a>
+                    <a href="#">${nav[2]}</a>
+                    <a href="#">${nav[3]}</a>
+                    <a href="#">${nav[4]}</a>
+                    <a href="#">${nav[5]}</a>
+                    <a href="#">${nav[6]}</a>
+                </div>
+            </nav>
+        </div>`;
+
+    
+        document.querySelector('header').innerHTML = HTML;
+
+        return HTML;
+    }
+   
+
 
 renderHeader(nav);
