3f4806c7322527127281ccceeba4f1b85e776d28
webdevelopment4everybody <56918351+webdevelopment4everybody@users.noreply.github.com>
2020-04-22 22:44:22 +0300

    initial html structure

--- js/my-portfolio.js
+++ js/my-portfolio.js
@@ -2,13 +2,34 @@
 
 function renderPortfolio(list){
     let HTML = '';
+    let filterHTML='';
+    let galleryHTML='';
   
     //sugeneruoti filtravima
-
+    filterHTML =`<div class="item">filter</div>
+            <div class="item">filter</div>
+            <div class="item">filter</div>
+            <div class="item">filter</div>
+            <div class="item">filter</div>`;
     //sugeneruoti darbus
-    
+
+    for(let i=0; i<list.length;i++){
+        const work=list[i];
+        galleryHTML += `<div class="item">
+                            <img src="./img/portfolio/${work.photo}" alt="${work.photo.alt}">
+                        </div>`;
+
+    }
     //apjungti viska i vientisa HTML
 
+    HTML+= `<div class="gallery">
+                <div class="filter"> 
+                ${filterHTML}
+                </div>
+                <div class="list"> 
+                ${galleryHTML}
+                </div>  
+            </div>`;
     //Pilna HTML iterpiame i DOM
     const DOMgallery = document.querySelector('#portfolio-gallery');
     DOMgallery.innerHTML = HTML;
