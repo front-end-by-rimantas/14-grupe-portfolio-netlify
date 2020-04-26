54d4433a37c93f58cda286ce9bf238a0fc22647a
webdevelopment4everybody <56918351+webdevelopment4everybody@users.noreply.github.com>
2020-04-26 17:46:19 +0300

    filtruoja, bet reiktu pagrazinti

--- js/my-portfolio.js
+++ js/my-portfolio.js
@@ -5,17 +5,28 @@ function renderPortfolio(list){
     let filterHTML='';
     let galleryHTML='';
   
+      //surenkame unikalius tagus
+      let uniqueTags = [];
+      for(let i=0; i<list.length; i++){
+          const tags = list[i].tags;
+          for(let t=0; t<tags.length;t++){
+              if(uniqueTags.indexOf(tags[t].toLowerCase()) === -1){
+                  uniqueTags.push(tags[t].toLowerCase());
+              }
+          }
+      }
+      
     //sugeneruoti filtravima
-    filterHTML =`<div class="item">filter</div>
-            <div class="item">filter</div>
-            <div class="item">filter</div>
-            <div class="item">filter</div>
-            <div class="item">filter</div>`;
+    filterHTML =`<div class="item active">All</div>`
+    for(let i=0;i<uniqueTags.length; i++){
+        filterHTML+=`<div class="item ">${uniqueTags[i]}</div>`
+    };
     //sugeneruoti darbus
 
     for(let i=0; i<list.length;i++){
         const work=list[i];
-        galleryHTML += `<div class="item size-${work.size}">
+        galleryHTML += `<div class="item size-${work.size}"
+        data-tags = "${work.tags}">
                             <img src="./img/portfolio/${work.photo}" alt="${work.title}">
                             <div class="hover"> 
                                 <div class = "text">
@@ -50,8 +61,41 @@ function renderPortfolio(list){
     DOMgallery.innerHTML = HTML;
 
     //prikabinti event listenerius, kad galetu filtruoti darbus
+ 
+    const filters = DOMgallery.querySelectorAll('.filter > .item');
+        for(let i=0; i<filters.length; i++){
+            const element=filters[i];
+            element.addEventListener('click', filterGallery);
+        };        
+        return;
+}
+function filterGallery(event){
+    document.querySelector('.filter > .item.active').classList.remove('active');
+    event.target.classList.add('active');
+    const filterTag = event.target.textContent;
+    const works = document.querySelectorAll('.gallery > .list > .item');
+    if(filterTag === 'All'){
+        for(let i=0; i<works.length; i++){
+           works[i].classList.remove('hide'); 
+            
+        }
+        return;
+    }
     
-    return;
+    for(let i=0; i<works.length; i++){
+        const work = works[i];
+       
+        const hasTags = work.dataset.tags.split(',').indexOf(filterTag);
+        if(hasTags >=0){
+            work.classList.remove('hide');
+        }else{
+            work.classList.add('hide');
+        }
+        
+    }
+    
+    
+    
 }
 
 renderPortfolio(works);
