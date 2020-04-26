f5197231506a8f1f691cfa2783521204d64e4545
Every1CanWriteACode <61137980+Every1CanWriteACode@users.noreply.github.com>
2020-04-26 11:50:34 +0300

    Initial commit

---
+++ js/achievements-data.js
@@ -0,0 +1,46 @@
+// Achievements
+
+// const data = [
+//     {
+//         simbol: 'heart',
+//         number: 738,
+//         text: 'Happy clients'
+//     },
+//     {
+//         simbol: 'check-square-o',
+//         number: 102,
+//         text: 'Succesful projects'
+//     },
+//     {
+//         simbol: 'trophy',
+//         number: 06,
+//         text: 'Award Recieved'
+//     },
+//     {
+//         simbol: 'coffee',
+//         number: 511,
+//         text: 'Cups of Tea'
+//     }
+// ];
+const data =[
+    {
+        simbol: 'heart',
+        number: 738,
+        text: 'Happy-clients'
+    },
+    {
+        simbol: 'check-square-o',
+        number: 102,
+        text: 'Succesfull-projects'
+    },
+    {
+        simbol: 'trophy',
+        number: 6,
+        text: 'Awards received '
+    },
+    {
+        simbol: 'coffee',
+        number: 511,
+        text: 'Cups of tea'
+    }
+];
--- js/data.js
+++ js/data.js
@@ -46,4 +46,3 @@ const skills2 =[
 ];
 
 
-
