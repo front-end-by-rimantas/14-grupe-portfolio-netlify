458545012e52e6db47566e06c09852a2f5af4107
Every1CanWriteACode <61137980+Every1CanWriteACode@users.noreply.github.com>
2020-04-26 12:26:54 +0300

    Achievements count up (?) #26

--- js/achievements.js
+++ js/achievements.js
@@ -62,7 +62,7 @@ function renderData( numberList ) {
         renderData( data );
 
         window.addEventListener('scroll', ()=>{
-            if(window.pageYOffset > 2040){
+            if(window.pageYOffset > 2080){
     achievementCounter('#data');
 }
 });
